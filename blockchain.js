638fa356da881425fd3249b62c682bde26f8e16a
Geek838 <geek838@gmail.com>
2020-02-11 22:52:42 +0400

    add chain validation function

--- blockchain.js
+++ blockchain.js
@@ -32,6 +32,23 @@ class BlockChain {
         newblock.hash = newblock.calcHash();
         this.chain.push(newblock);
     }
+    isValidBlock(){
+        for (let i=1; i<this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if (currentBlock.previousHash != previousBlock.hash){
+                return false;
+
+            } else if (currentBlock.hash != currentBlock.calcHash()){
+                return  false;
+            } else  {
+                return  true;
+            }
+
+        }
+    }
+
+
 }
 
 let firstCrypto = new BlockChain;
@@ -40,5 +57,12 @@ firstCrypto.createBlock(new Block('01', '11/02/2020', {
     from: 'X',
     to: 'Y'
 }));
-
+firstCrypto.createBlock(new Block('02', '12/02/2020', {
+    transaction: 15,
+    from: 'Z',
+    to: 'W'
+}));
+// trying to modify data to check chain checker function
+firstCrypto.chain[1].date = '27/12/2019';
+console.log('this chain is valid = ' + firstCrypto.isValidBlock());
 console.log(JSON.stringify(firstCrypto, null, 4));
